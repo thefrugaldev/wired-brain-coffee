1eeebfc59d2c1908af5567b916446cf54ac0bb1e
Clayton <corman.dev@gmail.com>
2020-12-31 12:44:35 -0500

    :crystal_ball: add data for seasonal gift card cells

---
+++ WiredBrainCoffee/Scenes/GiftViewController/GiftCardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GiftCardCollectionViewCell.swift
+//  WiredBrainCoffee
+//
+//  Created by Clayton Orman on 12/31/20.
+//  Copyright © 2020 Mark Moeykens. All rights reserved.
+//
+
+import UIKit
+
+class GiftCardCollectionViewCell: UICollectionViewCell {
+    
+}
