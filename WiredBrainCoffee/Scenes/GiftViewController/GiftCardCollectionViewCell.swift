59d125a0350027eb4154ec87bccf253114efc6a5
Clayton <corman.dev@gmail.com>
2021-01-02 11:20:39 -0500

    :art: update aspect rations and rounded corners for seasonal gift cards

--- WiredBrainCoffee/Scenes/GiftViewController/GiftCardCollectionViewCell.swift
+++ WiredBrainCoffee/Scenes/GiftViewController/GiftCardCollectionViewCell.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class GiftCardCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var giftCardImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 10
+        giftCardImageView.layer.cornerRadius = layer.cornerRadius
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+    }
+    
+    func setup(giftCardModel: GiftCardModel) {
+        giftCardImageView.image = giftCardModel.image
+    }
 }
